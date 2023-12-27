ccedc596a37c4e3f90a1e4daf4dd3aa9338c2182
Amit Mondal <74509859+Charlie-logan@users.noreply.github.com>
2023-12-27 15:49:03 +0530

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-let a=document.getElementById('cir1');
-let d=document.getElementById('img1');
-let b=document.getElementById('p1');
-let c=document.getElementById('btn');
-
-c.addEventListener('click',function(){
-    b.appendChild(d);
-    b.appendChild(a);
-});
